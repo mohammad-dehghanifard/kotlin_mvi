7ec4e0a580491af0d77982c27625f870b09f088d
mohammad <m.deheghanifard@gmail.com>
2024-03-06 14:01:13 +0330

    completed postApi Provider

--- app/src/main/java/ir/dehghanifard/kotlin_mvi/api/PostApi.kt
+++ app/src/main/java/ir/dehghanifard/kotlin_mvi/api/PostApi.kt
@@ -1,7 +1,27 @@
 package ir.dehghanifard.kotlin_mvi.api
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import ir.dehghanifard.kotlin_mvi.models.Post
+import kotlinx.serialization.json.Json
 
 interface PostApi {
     suspend fun getAllPostApi() : List<Post>
+
+    //provide api
+    companion object {
+        private val httpClient = HttpClient(Android){
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        this.ignoreUnknownKeys = true
+                    }
+                )
+            }
+        }
+
+        fun providePostApi() : PostApi = PostApiImpl(httpClient)
+    }
 }
